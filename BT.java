e371359891736a60124c68ebda7c9d6f8a3e2b4e
joberto14 <jav141institutonacional.com>
2026-04-19 20:52:27 -0400

    add: "un monton de cuestiones XD toda la logica del backtraking: funcion de agregacion con recursion funcion isValid para ver si la solucion es valida funcion calcular costo para no llenar el heap ya que corri el programa una vez y el heap se lleno "

--- BT.java
+++ BT.java
@@ -4,33 +4,39 @@ import java.io.StreamTokenizer;
 import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
+
 import java.util.Comparator;
 
+// clase avion para guardar datos
+class Avion {
+	int Ei, Pi, Li, dom, i;
+	double Ci, Ck;
+	int[] tau;
+
+	Avion(int n) {
+		tau = new int[n];
+	}
+
+	public int getDom(){
+		return dom;
+	}
+	public void setDom(){
+		dom = Li - Ei;
+	} 
+}
 public class BT {
+	static double mejorCosto = Double.MAX_VALUE;
+
+	static long tiempoDeSolucionEncontrada;
+	static int solucionesEncontradas = 0;
+	static int nodos = 0;
+	static int n;
+	static Avion[] aviones;
+	static long inicio;
     public static void main(String[] args) throws IOException {
-
-		// clase avion para guardar datos
-		class Avion {
-			int Ei, Pi, Li, dom, i;
-			double Ci, Ck;
-			int[] tau;
-
-			Avion(int n) {
-				tau = new int[n];
-			}
-
-			public int getDom(){
-				return dom;
-			}
-			public void setDom(){
-				dom = Li - Ei;
-			} 
-		}
-
-		
-		Avion[] aviones;
-		int n;
 		
 		// lectura de standar input
         try (BufferedReader br = Files.newBufferedReader(Path.of("case1.txt"))) {
@@ -59,11 +65,8 @@ public class BT {
 				}
 			}
         }
-		
 		// fin lectura standar input
 		
-		// solucion es un arreglo, pos[0] -> tiempo de aterrizaje avion 1
-		int[] solucion = new int[n];
 		
 		// heuristica de seleccion de variable 
 		// se usa Dom -> la que tiene el dominio menor, es decir, Li - Ei < a las demas
@@ -71,15 +74,98 @@ public class BT {
 		Arrays.sort(aviones, Comparator.comparingInt(Avion::getDom));
 		
 		// fin heuristica, aviones ordenados
-		
-		
-		
-		
-		
-		
+
 		//System.out.println(aviones[0].i);
+		ArrayList<ArrayList<Integer>> Soluciones = new ArrayList<>();
+		ArrayList<Integer> solparcial= new ArrayList<>();
 
+		
+		
+		//tiempo inicio
+		inicio = System.nanoTime();
+		long inicioEjecucion = System.nanoTime();
 
+		// ------------------------------------------- hasta acamisma implementacion para FC pero cambiar las funciones ---------------------------------
+		
+		// algoritmo principal
+		asignarTiempoAterrizaje(0,Soluciones, solparcial, aviones);
+
+		// tiempo fin
+		long fin = System.nanoTime();
+		long tiempo = fin - inicioEjecucion;
+
+		System.out.println("cantidad de nodos creados:");
+		System.out.println(nodos);
+		System.out.println("Tiempo de Ejecucion:");
+		System.out.println(tiempo);
+		
 
     }
+
+	public static void asignarTiempoAterrizaje(int index, ArrayList<ArrayList<Integer>> Soluciones, ArrayList<Integer> actual, Avion[] listaAviones){
+		nodos = nodos + 1;
+		//System.out.println(nodos);
+		
+		// caso base
+		if(n == index){
+			
+			double costoDeLaSolucion = calcularCosto(actual);
+			// solo metemos la solucion si mejora el costo actual para no llenar el heap
+			if(costoDeLaSolucion < mejorCosto){
+				mejorCosto = costoDeLaSolucion;
+				
+				Soluciones.add(new ArrayList<>(actual));
+				double segundos = (System.nanoTime() - inicio) / 1_000_000.0;
+	
+				System.out.printf("Solución encontrada en %.4f milisegundos%n", segundos);
+				return;
+			}
+			return;
+		}
+		
+		Avion curr = listaAviones[index]; 
+		
+		// debug
+		//System.out.println("entre a asignarTiempoAterrizaje");
+		//isValid(index, 2, listaAviones);
+		
+		for (int t = curr.Ei; t <= curr.Li; t++) {
+			if (isValid(index, actual, t ,listaAviones)) {
+				actual.add(t); // elegir
+            	asignarTiempoAterrizaje(index + 1, Soluciones, actual, listaAviones); 
+				actual.remove(actual.size() - 1);
+            }
+		}
+	}
+
+	static boolean isValid(int idx, ArrayList<Integer> actual, int tiempoAterrizaje, Avion[] listaAviones) {
+		//System.out.println("entre a isValid y mi idx es:");
+		//System.out.println(idx);
+
+        for (int j = 0; j < idx; j++) {
+			int tj = actual.get(j);
+			Avion prev = listaAviones[j];   
+			Avion curr = listaAviones[idx]; 
+
+			if (tiempoAterrizaje >= tj) {
+				
+				if (tiempoAterrizaje - tj < prev.tau[curr.i]) return false; 
+			} else {
+				
+				if (tj - tiempoAterrizaje < curr.tau[prev.i]) return false;
+			}
+		}
+    	return true;
+	}
+
+	static double calcularCosto(ArrayList<Integer> solAEvaluar){
+		double costo = 0;
+		for (int k = 0; k < n; k++) {
+			int t = solAEvaluar.get(k);   // tiempo asignado al avión k
+			Avion a = aviones[k];
+
+			costo = Math.max(a.Ci * (a.Pi - t), 0) + Math.max(a.Ck * (t - a.Pi), 0);
+		}
+		return costo;
+	}
 }
--- IAusos.md
+++ IAusos.md
@@ -2,4 +2,10 @@ para buscar como leer archivos txt en java
 
 Le pregunte como podria guardar losdatos del standar input, me dio la el uso de la clase avion y el uso de stream tokenizer para la lectura
 
-para explicar las heuristicas de selccion de valo
+para explicar las heuristicas de selccion de valor
+
+Una vez establecido un esqueleto de la funcion asignarTiempoAterrizaje ayudo a terminarla, haciendo que reciba una ArrayList para agregar al arreglo de posbles soluciones, como tambien arregalr la logica del backtrackting con la linea "actual.remove(actual.size() - 1);" para eliminar el valor en caso de no ser parte de una solucion invalida
+
+Para hacer la funcion isValid
+
+Para hacer el mensaje output de la solucion encontrada
